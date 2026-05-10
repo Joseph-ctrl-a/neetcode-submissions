c10693a23de871dbba59fad0339b81533e6cf11e
Jospeh Ogbomo <josephogbo18@outlook.com>
2026-05-10 21:39:25 +0100

    Add: longest-consecutive-sequence - submission-16

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-16.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+        const seen = new Set(nums)
+        let res = 0 
+        for (const num of seen ) {
+            if (seen.has(num - 1)) continue
+            let i = 0
+            while (seen.has(num + i)) {
+                i++
+            }
+            res = Math.max(res, i)
+        }
+        return res
+    }
+}
