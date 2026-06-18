26def458895c87c2c9d75cfef9ba1e81b4d9db29
Jospeh Ogbomo <josephogbo18@outlook.com>
2026-06-18 18:47:11 +0100

    Add: delete-node-in-a-bst - submission-0

---
+++ Data Structures & Algorithms/delete-node-in-a-bst/submission-0.js
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @param {number} key
+     * @return {TreeNode}
+     */
+    deleteNode(root, key) {
+
+        if (!root) return root
+
+        if (key > root.val) root.right = this.deleteNode(root.right, key)
+        else if (key < root.val) root.left = this.deleteNode(root.left, key)
+        else {
+            if (!root.left) {
+                return root.right
+            }
+            else if (!root.right) {
+                return root.left
+            }
+
+            let curr = root.right
+            while (curr.left) {
+                curr = curr.left
+            }
+            const successorVal = curr.val
+            root.val = curr.val
+            root.right = this.deleteNode(root.right, successorVal)
+        } 
+        return root
+
+    }
+}
